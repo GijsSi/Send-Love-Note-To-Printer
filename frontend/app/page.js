708d7a21d0831a383acea9b910e01f030a6b5b12
Gijs Siemer <gijs.siemer@me.com>
2024-03-24 16:33:09 +0100

    Update page.js

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -10,7 +10,8 @@ export default function Home() {
   const sendMessage = async () => {
     try {
       // Send the message to the backend API to print it out - USE YOUR OWN URL/IP address
-      await axios.post('https://XXX.XXXX.XXXX/print', { message });
+      await axios.post('https://api.katjalovesgijs.love/print', { message });
+      console.log('Message sent:', message)
       setMessage('');
       setIsSent(true);
     } catch (error) {
@@ -20,25 +21,25 @@ export default function Home() {
 
 return (
   <main className="flex min-h-screen flex-col items-center p-24 bg-rose-100">
-    {isSent ? (
-      <Image src="/love-you-i-love-you.gif" width={300} height={300} />
-    ) : (
-      <>
-        <Image src="/baby-bear-brown.gif" width={300} height={300} />
-        <h1 className="font-semibold">Send me a little note</h1>
-        <textarea
-          value={message}
-          onChange={e => setMessage(e.target.value)}
-          className="mt-4 p-2 border-2 pb-8 border-rose-200 rounded-md"
-        />
-        <button
-          onClick={sendMessage}
-          className="mt-4 px-4 py-2  bg-rose-500 text-white rounded-md"
-        >
-          Send
-        </button>
-      </>
-    )}
-  </main>
+  {isSent ? (
+    <Image src="/love-you-i-love-you.gif" width={300} height={300} />
+  ) : (
+    <>
+      <Image src="/baby-bear-brown.gif" width={300} height={300} />
+      <h1 className="font-semibold">Send me a little note</h1>
+      <textarea
+        value={message}
+        onChange={e => setMessage(e.target.value)}
+        className="mt-4 p-2 border-2 border-rose-200 rounded-md"
+      />
+      <button
+        onClick={sendMessage}
+        className="mt-4 px-4 py-2 bg-rose-500 text-white rounded-md"
+      >
+        Send
+      </button>
+    </>
+  )}
+</main>
 );
 }
